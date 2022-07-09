3400f0dee70263075c1d39f30ae8ed05cb70a289
Rui En <neoruien@u.nus.edu>
2022-07-09 22:26:09 +0800

    Add more react router edits

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import HomePage from "./pages/HomePage.js";
 import SignupPage from "./pages/SignupPage.js";
 
 export const App = () => {
-    return (
-        <div className="App">
-            <Switch>
-                <Route exact path="/">
-                    <HomePage />
-                </Route>
-                <Route exact path="/signup">
-                    <SignupPage />
-                </Route>
-            </Switch>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/signup">
+          <SignupPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
--- src/Theme.js
+++ src/Theme.js
@@ -1,4 +1,4 @@
 export const Colors = {
-    primary: "#61435D",
-    secondary: "#DFD9DF",
+  primary: "#61435D",
+  secondary: "#DFD9DF",
 };
