f55c65c401bb94ec52b7564e51b1d042892fa9b9
Rui En <neoruien@u.nus.edu>
2022-07-10 03:14:56 +0800

    Add signup button

--- src/Theme.js
+++ src/Theme.js
@@ -2,4 +2,5 @@ export const Colors = {
   primary: "#61435D",
   secondary: "#DFD9DF",
   red: "#AD6363",
+  offWhite: "#E0E1E2",
 };
--- src/pages/HomePageElements.js
+++ src/pages/HomePageElements.js
@@ -3,9 +3,17 @@ import { Colors } from "../Theme";
 
 export const Main = styled.div`
   background-color: ${Colors.primary};
+  width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+
+export const BottomText = styled.p`
+  margin: 2rem;
+  text-decoration: none;
+  color: white;
+  text-decoration: underline;
 `;
